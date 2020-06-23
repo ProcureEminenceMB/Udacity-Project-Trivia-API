7487fdce9d06dfab087d5fb1fa004c7a9b0145eb
Michael Bigelow <procureeminence@gmail.com>
2020-06-23 01:46:26 -0400

    feat: Add tests for error handlers

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -18,17 +18,63 @@ class TriviaTestCase(unittest.TestCase):
 		self.database_path = "postgres://{}/{}".format('admin:admin@localhost:5432', self.database_name)
 		setup_db(self.app, self.database_path)
 
-		# binds the app to the current context
+		# Sample question
+		self.new_question = {
+			'question': 'What type of paint does Bob Ross use?',
+			'answer': 'Oil',
+			'difficulty': '2',
+			'category': '2'
+		}
+
+		# Binds the app to the current context
 		with self.app.app_context():
 			self.db = SQLAlchemy()
 			self.db.init_app(self.app)
-			# create all tables
-			self.db.create_all()
+			self.db.create_all()# Create all tables
 	
 	def tearDown(self):
 		"""Executed after reach test"""
 		pass
 
+	# Test error handlers
+	def test_400_error(self):
+		res = self.client().post('/questions', json={})
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 400)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'Bad Request')
+
+	def test_404_error(self):
+		res = self.client().get('/category')
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 404)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'Not Found')
+
+	def test_422_error(self):
+		invalid_question = {
+			'question': 'Where do you look to see the sky?',
+			'answer': 'Down',
+			'difficulty': 'F',
+			'category': '1'
+		}
+		res = self.client().post('/questions', json=invalid_question)
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 422)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'Unprocessable')
+
+	def test_405_error(self):
+		res = self.client().put('/questions')
+		data = json.loads(res.data)
+
+		self.assertEqual(res.status_code, 405)
+		self.assertEqual(data['success'], False)
+		self.assertEqual(data['message'], 'Method Not Allowed')
+	# END Test error handlers
 # Make the tests conveniently executable
 if __name__ == "__main__":
 	unittest.main()
